6c89d627ffcc0cf076009539a929e19e385e6746
Yizhou Liu <yizhou.liu.mail@gmail.com>
2020-07-21 14:45:58 -0700

    added Header outside of Switch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import {Route, Switch} from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
+import Header from './components/header/header.component'
 import './App.css';
 
 function App() {
   return (
     <div className="">
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
