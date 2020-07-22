e4d944053b23379e4811803d0f0a7096ed01f7fd
Yizhou Liu <yizhou.liu.mail@gmail.com>
2020-07-22 11:00:26 -0700

    fixed CustomButton Google Sign in issue

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -57,7 +57,7 @@ export class SignIn extends Component {
             required />
           <div className='buttons'>
             <CustomButton type="submit">Sign In</CustomButton>
-            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>Sign In With Google</CustomButton>
+            <CustomButton type="button" onClick={signInWithGoogle} isGoogleSignIn>Sign In With Google</CustomButton>
           </div>
         </form>
       </div>
