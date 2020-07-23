26bb0ffa0cb49d1f4e0be953612603e5593f657e
Yizhou Liu <yizhou.liu.mail@gmail.com>
2020-07-23 10:50:32 -0700

    created empty checkout page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header.component'
 import SignInAndSignUpPage from './pages/signin-and-signup/signin-and-signup.component.jsx'
+import CheckoutPage from './pages/checkout/checkout.component'
 
 import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 import { setCurrentUser } from './redux/user/user.actions'
@@ -46,6 +47,7 @@ class App extends React.Component {
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
           <Route exact path='/signin' render={() => this.props.currentUser ? (<Redirect to='/' />) : (<SignInAndSignUpPage />)} />
+          <Route exact spath='/checkout' component={CheckoutPage} />
         </Switch>
       </div>
     );
