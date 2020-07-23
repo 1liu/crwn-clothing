8d21a8b2ce30741a6f1b68ecd693b3a3a1460365
Yizhou Liu <yizhou.liu.mail@gmail.com>
2020-07-23 15:38:21 -0700

    created directory reducer and managed directory sections with redux

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,29 +1,26 @@
 
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { selectDirectorySections } from '../../redux/directory/directory.selectors'
+import { createStructuredSelector } from 'reselect'
+
 import MenuItem from '../menu-item/menu-item.component'
-import './sections.data'
 import './directory.styles.scss'
-import SECTIONS_DATA from './sections.data';
 
-export class Directory extends Component {
-  constructor() {
-    super();
+const Directory = ({ sections }) => {
 
-    this.state = {
-      sections: SECTIONS_DATA
-    }
-  }
-
-
-  render() {
-    return (
-      <div className='directory-menu'>
-        {this.state.sections.map(({ id, ...otherSectionProps }) => (
-          <MenuItem key={id} {...otherSectionProps} />
-        ))}
-      </div>
-    );
-  }
+  return (
+    <div className='directory-menu'>
+      {sections.map(({ id, ...otherSectionProps }) => (
+        <MenuItem key={id} {...otherSectionProps} />
+      ))}
+    </div>
+  );
 }
 
-export default Directory
+
+const mapStateToProps = createStructuredSelector({
+  sections: selectDirectorySections
+})
+
+export default connect(mapStateToProps)(Directory)
