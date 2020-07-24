157ca2b835bfb4b0d733a6997250c6d8da7913e0
Yizhou Liu <yizhou.liu.mail@gmail.com>
2020-07-23 23:29:18 -0700

    connected CollectionPage to redux and created selectCollection selector

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { connect } from 'react-redux'
+
+import { selectCollection } from '../../redux/shop/shop.selectors'
 
 import CollectionItem from '../../components/collection-item/collection-item.component'
 
@@ -13,4 +16,8 @@ const CollectionPage = ({ match }) => {
   )
 }
 
-export default CollectionPage
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state)
+})
+
+export default connect(mapStateToProps)(CollectionPage)
