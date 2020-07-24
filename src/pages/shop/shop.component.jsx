c3f644d3677aa9e44d21cca0633103f0c005c5ed
Yizhou Liu <yizhou.liu.mail@gmail.com>
2020-07-23 21:33:14 -0700

    utilized redux reducer to manage shop collections data

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,26 +1,24 @@
-import React, { Component } from 'react'
-import SHOP_DATA from './shop.data'
+import React from 'react'
+import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
+//import SHOP_DATA from './shop.data'
 import CollectionPreview from '../../components/collection-preview/collection-preview.component'
+import { selectShopCollections } from '../../redux/shop/shop.selectors'
+const ShopPage = ({ collections }) => {
 
-class ShopPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      collections: SHOP_DATA
-    }
-  }
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className='shop-page'>
-        {
-          collections.map(({ id, ...otherCollectionProps }) => (
-            <CollectionPreview key={id} {...otherCollectionProps} />
-          ))
-        }
-      </div>
-    )
-  }
+  return (
+    <div className='shop-page'>
+      {
+        collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))
+      }
+    </div>
+  )
+
 }
 
-export default ShopPage;
+const mapStateToProps = createStructuredSelector({
+  collections: selectShopCollections
+})
+export default connect(mapStateToProps)(ShopPage);
