2ac92a0651f3e123b8c81d1c3e3cc818ab154fa2
Yizhou Liu <yizhou.liu.mail@gmail.com>
2020-08-03 14:33:53 -0700

    added compression library for gzip for heroku

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser')
 const path = require('path');
 const { executionAsyncResource } = require('async_hooks');
+const compression = require('compression')
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
@@ -14,7 +15,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(compression());
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
